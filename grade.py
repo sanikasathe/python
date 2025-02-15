7f2eb73bf79d79d87d705c9480208422cb73b222
sanikasathe <sanikasathe32@gmail.com>
2025-02-15 18:28:49 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,11 @@
+marks=float(input("enter marks"))
+if(marks>=90):
+    print("grade:A")
+elif(marks>=80):
+    print("grade:B")
+elif(marks>=70):
+    print("grade:C")
+elif(marks>=60):
+    print("grade:D")
+else:
+    print("grade:E")
