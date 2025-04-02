f577e9baece4bbaf283d7b62c7cbbad717cec0ee
sanikasathe <sanikasathe32@gmail.com>
2025-04-02 10:19:56 +0530

    Create streetlight_number.py

---
+++ streetlight_number.py
@@ -0,0 +1,6 @@
+#Q2
+streetlight_number=int(input("enter the streetlight number"))
+if(streetlight_number%2==0):
+    print("its even number so turned on at night")
+else:
+    print("its odd number so remains off at night")
